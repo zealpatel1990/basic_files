7b7921dccaf3f371780977d6874131f6a3a5c42a
zeal <zealpatel1990@gmail.com>
2018-03-13 09:38:53 +0000

    notes updated

--- r.pi/off.py
+++ r.pi/off.py
@@ -4,7 +4,6 @@ def off():
 	sys.path.append('/storage/.kodi/addons/python.RPi.GPIO/lib/')
 	import RPi.GPIO as GPIO
 	GPIO.setmode(GPIO.BOARD)
-	i=[3,5,7,8,10,11,12,13,15,16,18,19,21,22,23,24,26]
 	for x in range(0,41):
 		try:
 			GPIO.setup(x,GPIO.OUT)
@@ -13,3 +12,4 @@ def off():
 			continue
 off()
 GPIO.cleanup()
+print "All GPIO pins are turned Off."
