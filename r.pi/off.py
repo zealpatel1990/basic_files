5869a3c621f4fa777b5c72054db44430a4fdeaf6
Zeal <zealpatel1990@gmail.com>
2020-01-29 13:39:17 -0600

    Update off.py

--- r.pi/off.py
+++ r.pi/off.py
@@ -1,6 +1,9 @@
+# this script works for python 2 and python 3 ONLY ON RASPBERRY PI
 import RPi.GPIO as GPIO
 def off():
 	import sys
+	# below line is for people who are using openelec or similar os for media pupose as well as for other purpose
+	#you don't have to worry about is you are using raspbian
 	sys.path.append('/storage/.kodi/addons/python.RPi.GPIO/lib/')
 	import RPi.GPIO as GPIO
 	GPIO.setmode(GPIO.BOARD)
@@ -10,6 +13,7 @@ def off():
 			GPIO.output(x,0)
 		except:
 			continue
+	GPIO.cleanup()
+	print ("All GPIO pins are turned Off.")
+
 off()
-GPIO.cleanup()
-print "All GPIO pins are turned Off."
